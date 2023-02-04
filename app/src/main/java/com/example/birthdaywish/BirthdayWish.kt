b17cb8e23f590a4199cc56b68a51e42dcfab7115
Rajan <rjha2.718@gmail.com>
2023-02-04 05:55:33 +0530

    FirstCommit

---
+++ app/src/main/java/com/example/birthdaywish/BirthdayWish.kt
@@ -0,0 +1,11 @@
+package com.example.birthdaywish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayWish : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_wish)
+    }
+}
---
+++ app/src/main/java/com/example/birthdaywish/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.birthdaywish
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.example.birthdaywish.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun createBirthdaycard(view: View) {
+
+
+        Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+    }
+
+}
